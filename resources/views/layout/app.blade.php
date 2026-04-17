868d1d86f33ce2e4fb78988161d0e4d6bcd3d6c0
Oscar Ruiz <oscarruiz2614@gmail.com>
2026-04-17 15:22:47 -0500

    feat: :sparkles: isntalar filament

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="index, follow">
+    <title>13 Semana Técnica Internacional de Petróleos, Gas y Energia | USCO</title>
+    <meta name="description" content="Participa en la Semana Técnica Internacional de Petróleo y Gas en la USCO. Conferencias, networking y expertos del sector energético.">
+    <meta name="keywords" content="petróleo y gas, evento petrolero, ingeniería de petróleos, USCO, semana técnica">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
